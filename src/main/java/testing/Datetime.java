849bc4bd66393c0a96fcf8b88ce8f14352aa2f69
Lukasz <uzytkownik@kursgit.pl>
2020-07-11 18:09:29 +0200

    Foreach and dates

--- src/main/java/testing/Datetime.java
+++ src/main/java/testing/Datetime.java
@@ -1,4 +1,20 @@
 package testing;
 
+
+import java.time.LocalDate;
+
 public class Datetime {
+
+    public static void dateNow(){
+        LocalDate currentDate = LocalDate.now();
+        System.out.println(currentDate);
+    }
+    public static void dateTimeInTokyo(){
+        
+    }
+
+    public static void main(String[] args) {
+        dateNow();
+    }
+
 }
