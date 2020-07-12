fe8e891c3f66119002e339dfce86bedbd37186e3
Lukasz <uzytkownik@kursgit.pl>
2020-07-12 12:23:40 +0200

    zadanie 7

--- src/main/java/zadaniasobota/Zadanie7.java
+++ src/main/java/zadaniasobota/Zadanie7.java
@@ -1,5 +1,17 @@
 package zadaniasobota;
 
 public class Zadanie7 {
-    
+    public static int[] arraySeries(int lenght, int firstNumber, int gap){
+        int[] array = new int[lenght];
+        array[0] = firstNumber;
+        for (int i = 1; i < lenght; i++){
+            array[i] = array[i-1] + gap;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] result = arraySeries(4,1,2);
+        Zadanie6.printTable(result);
+    }
 }
