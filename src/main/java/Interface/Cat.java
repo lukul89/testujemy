911de83c8156bbb3f41472839fe125a06df4e748
Lukasz <uzytkownik@kursgit.pl>
2020-07-18 14:45:52 +0200

    Animal interface for aniaml sound update

--- src/main/java/Interface/Cat.java
+++ src/main/java/Interface/Cat.java
@@ -1,14 +1,30 @@
 package Interface;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter             // ustawienie gettera przez lombook
+@Setter             // ustawienie settera przez lombook
 public class Cat implements Animal {
     private String catSound;
+    private int age;
+
 
     public Cat(){
         this.catSound = "Meow";
     }
     public Cat(String ownSound){
+
         this.catSound = ownSound;
     }
+
+    //public int getAge() {                 ustawianie getera po public zaczac pisac get i samo sie ustawi
+    //    return age;
+   // }
+
+   // public void setAge(int age) {         ustawianie settera po public zaczac pisac set i samo sie ustawi
+   //     this.age = age;
+   // }
 
     @Override
     public String makeSound() {
--- src/main/java/Interface/Main.java
+++ src/main/java/Interface/Main.java
@@ -1,19 +1,47 @@
 package Interface;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Cat garfield = new Cat();
-        Cat filemon = new Cat("Mrau");
+//        Cat garfield = new Cat();
+//        Cat filemon = new Cat("Mrau");
        // System.out.println(Cat.testMakeSound());
 
         //String garfieldSound = garfield.makeSound();
         //System.out.println(garfieldSound);
         //System.out.println(filemon.makeSound());
 
-        Cow mucka = new Cow("Moo");
-        System.out.println(mucka.makeSound());
+//        Cow mucka = new Cow("Moo");
+//        System.out.println(mucka.makeSound());
+//
+//        Dog reksio = new Dog("HauHau");
+//        System.out.println(reksio.makeSound());
+//
+//        filemon.setAge(5);
+//        System.out.println(filemon.getAge());
 
-        Dog reksio = new Dog("HauHau");
-        System.out.println(reksio.makeSound());
+        Cat garfield = new Cat();
+        Cat filemon = new Cat();
+        Cat teodor = new Cat();
+        Dog reksio = new Dog("Woof");
+        Dog reks = new Dog("Hau");
+        Cow mucka = new Cow("Mooo");
+
+
+        List<Animal> listOfAnimals = new ArrayList<>();
+        listOfAnimals.add(garfield);
+        listOfAnimals.add(filemon);
+        listOfAnimals.add(teodor);
+        listOfAnimals.add(reksio);
+        listOfAnimals.add(reks);
+        listOfAnimals.add(mucka);
+
+        for (Animal animal : listOfAnimals){
+            System.out.println(animal.makeSound());
+        }
+
+
     }
 }
